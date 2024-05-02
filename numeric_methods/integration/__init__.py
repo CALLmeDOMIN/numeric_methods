82773f948bdd77427e494218f3ca08de96aa6f0b
CALLmeDOMIN <dominiksieron13@gmail.com>
2024-05-02 18:35:08 +0200

    Feat: Integral approximation function

--- numeric_methods/integration/__init__.py
+++ numeric_methods/integration/__init__.py
@@ -2,6 +2,7 @@ from .simpson import simpson_formula
 from .trapezoidal import trapezoidal_formula
 from .gl import gauss_legendre_formula
 from .gl import gauss_legendre_formula_intervals
+from .approximation import approximate_integral
 
 __all__ = ['simpson_formula', 'trapezoidal_formula',
-           'gauss_legendre_formula', 'gauss_legendre_formula_intervals']
+           'gauss_legendre_formula', 'gauss_legendre_formula_intervals', 'approximate_integral']
---
+++ numeric_methods/integration/approximation.py
@@ -0,0 +1,30 @@
+from numeric_methods import gauss_legendre_formula, lu_decomposition
+from typing import Callable, List
+
+
+def approximate_integral(f: Callable[[float], float], base: List[any], start: float, end: float) -> List[float]:
+    """
+    Function to approximate the integral of a function using a polynomial base and the Gauss-Legendre formula.
+
+    Args:
+        f (Callable[[float], float]): Function to integrate.
+        base (List[any]): Base of the polynomial.
+        start (float): Start of the interval.
+        end (float): End of the interval.
+
+    Returns:
+        List[float]: Coefficients of the polynomial.
+    """
+    matrix = []
+    intercept = []
+
+    for i in range(len(base)):
+        row = []
+        for j in range(len(base)):
+            row.append(gauss_legendre_formula(
+                lambda x: x**(i + j), 4, start, end))
+        matrix.append(row)
+        intercept.append(gauss_legendre_formula(
+            lambda x: ((x ** i) * f(x)), 4, start, end))
+
+    return lu_decomposition(len(base), matrix, intercept)
